1b9bf68438db082d90eb6e0421935bb1316003e8
Rowee13 <roweeapor@gmail.com>
2025-07-02 18:20:11 +0800

    update url value in site config

--- constant/config.ts
+++ constant/config.ts
@@ -2,5 +2,5 @@ export const siteConfig = {
     title: 'Calculation Oauth Test App',
     description:
         'Quick test app for OAuth2.0 authentication with calculation functionality',
-    url: 'https://oauth-test-app.vercel.app',
+    url: 'https://oauth-calc-test-app.vercel.app',
 }
